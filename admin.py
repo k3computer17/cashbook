b6cc4e3ddce7dba20eea90618b7106edb6a48509
k3computer17 <89633689+k3computer17@users.noreply.github.com>
2026-08-24 21:04:55 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,13 +7,14 @@ class Admin:
         conn = sqlite3.connect('software_data.db')
         cursor = conn.cursor()
         
+        # ऑटोमैटिक यूजर आईडी जनरेट करना
         rand_num = random.randint(1000, 9999)
         user_id = f"USR{mobile[-4:]}{rand_num}" if mobile else f"USR{rand_num}"
         one_time_password = f"OTP@{random.randint(1000, 9999)}"
         
         try:
             cursor.execute('''
-                INSERT INTO users (user_id, password, role, active, name, father_name, aadhar_last4, mobile, email, address, city, district, state, pin)
+                INSERT OR REPLACE INTO users (user_id, password, role, active, name, father_name, aadhar_last4, mobile, email, address, city, district, state, pin)
                 VALUES (?, ?, ?, 1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             ''', (user_id, one_time_password, role, name, father_name, aadhar_last4, mobile, email, address, city, district, state, pin))
             
